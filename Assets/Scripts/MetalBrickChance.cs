36212436acc1a671e8daf576a580bb17a036c101
OwnedCoot <aobriencollege@gmail.com>
2024-11-25 14:54:23 -0500

    deleted the ONE LINE OF CODE CAUSING ALL OF MY PROBLEMS

--- Assets/Scripts/MetalBrickChance.cs
+++ Assets/Scripts/MetalBrickChance.cs
@@ -59,7 +59,6 @@ public class MetalBrickChance : MonoBehaviour
             }
             else
             {
-                scoreCounter.AddScore(pointsPerBrick);
                 DestroyBrick();
             }
         }
